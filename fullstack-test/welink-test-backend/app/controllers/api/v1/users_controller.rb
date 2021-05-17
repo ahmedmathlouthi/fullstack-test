7c5bfd111d730a7c8127e30071186155f2b7d832
ahmed mathlouthi <ahmed.mathlouthi.am@gmail.com>
2021-05-17 21:45:06 +0200

    remove unnecessary files

--- fullstack-test/welink-test-backend/app/controllers/api/v1/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::UsersController < ApplicationController
-  def index
-    render json: users, each_serializer: Api::V1::UserSerializer
-  end
-end
--- fullstack-test/welink-test-backend/app/serializers/api/v1/user_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  module V1
-    class UserSerializer < ActiveModel::Serializer
-      attributes :id, :firstname, :lastname, :email, :feedbacks
-    end
-  end
-end
